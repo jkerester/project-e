db150a7bbb466ef64dbc0f1e26b51d0792a442d7
James Kerester <jkerester@student.bridgew.edu>
2019-11-27 13:02:57 -0500

    Steps 1 and 2

---
+++ kernel.c
@@ -0,0 +1,327 @@
+//James Kerester and Molly Croke
+void printChar(char);
+void printString(char*);
+void readString(char*);
+void handleInterrupt21(int, int, int, int);
+void readSector(char*, int);
+void readFile(char*, char*,  int*);
+void executeProgram(char*);
+void terminate();
+void writeSector(char*, int);
+void deleteFile(char*);
+void writeFile(char*, char*, int);
+int compareFileNames(char*, char*, int, int);
+void handleTimerInterrupt(int, int); 
+
+//global variables
+int processActive[8];
+int processStackPointer[8];
+int currentProcess;
+
+
+main()
+{
+	int i;
+	int j;
+	makeInterrupt21();
+
+	for(i=0; i<8; i++) //initializing process table
+	{
+		processActive[i] = 0;
+	}
+	for(j=0; j<8; j++) //initializing process table
+	{
+		processStackPointer[j] = 0xff00;
+	}
+	currentProcess = -1; //initializing process table
+
+	makeTimerInterrupt();
+	interrupt(0x21, 4, "shell", 0, 0);
+
+	while(1);   /*hang up*/ 
+}
+//debugging function
+void printChar(char c)
+{
+	interrupt(0x10, 0xe*256 + c, 0, 0, 0);
+}
+
+void printString(char* chars)
+{
+	int i=0;
+	while(chars[i] != '\0')
+	{
+		interrupt(0x10, 0xe*256 + chars[i], 0, 0, 0);
+		i++;
+	} 
+}
+
+void readString(char* line)
+{
+	char keyPress = interrupt(0x16, 0, 0, 0, 0);
+	int i=0;
+	interrupt(0x10, 0xe*256 + keyPress, 0, 0, 0);//prints out first char
+
+	while(keyPress != 0xd) //0xd is the enter key
+	{
+		if(keyPress == 0x8 && i>0)//0x8 is the backspace key
+		{ 
+		        i--;//this decrements array index so that it won't
+			    //save the character deleted by backspace
+		}
+		else if(keyPress == 0x8)
+		{
+		//this handles if array index is already at 0
+			i=0;
+		}
+		else
+		{
+			//saves char to the array if not a backspace
+			line[i] = keyPress;
+			i++;
+		}
+		//gets next char
+		keyPress = interrupt(0x16, 0, 0, 0, 0);
+		//prints next char
+		interrupt(0x10, 0xe*256 + keyPress, 0, 0, 0);
+
+		//if next char is backspace, overwrite prior char with space 
+		//and backspace again for the screen
+		if(keyPress == 0x8)
+		{
+		interrupt(0x10, 0xe*256 + ' ', 0, 0, 0);
+		interrupt(0x10, 0xe*256 + 0x8, 0, 0, 0);
+		}
+	}
+	line[i] = 0xa; //line feed added to end of array
+	line[i+1] = 0x0; //"end of string" added to end of array
+	interrupt(0x10, 0xe*256 + 0xd, 0, 0, 0);//prints enter on screen
+	interrupt(0x10, 0xe*256 + 0xa, 0, 0, 0);//prints new line on screen
+}
+
+void handleInterrupt21( int ax, int bx, int cx, int dx)
+{
+	if(ax == 0){
+		printString(bx);
+	}else if(ax == 1){
+		readString(bx);
+	}else if(ax == 2){
+		readSector(bx, cx);
+	}else if (ax == 3){
+		readFile(bx, cx, dx);
+	}else if(ax == 4){
+		executeProgram(bx);
+	}else if(ax == 5){
+		terminate();
+	}else if(ax == 6){
+		writeSector(bx, cx);
+	}else if(ax == 7){
+		deleteFile(bx);
+	}else if(ax == 8){
+		writeFile(bx,cx,dx);
+	}else{
+		printString("Error, ax should be less than 9");
+	}
+}
+
+void readSector(char* buffer, int sector)
+{
+	int track = 0;
+	int head = 0;
+	int relSector = sector+1;
+	interrupt(0x13, 2*256+1, buffer, track*256+relSector,head*256+0x80);
+}
+
+void readFile(char* name, char* buffer, int* sectorsRead)
+{
+	int length = 0;
+	int entry;
+	int entryCount = 0;
+	char dir[512];
+	readSector(dir,2);
+
+
+	while(name[length] != '\0')
+	{
+		length++;
+
+	}
+
+	for(entry = 0; entry < 512; entry = entry + 32)
+	{
+		if(compareFileNames(name, dir, length, entryCount) == 1)
+		{
+			//use for loop to load the file
+			int e;
+			for(e=entry+6; e < (entry + 32); e++)
+			{   //entry now increments by one instead of 32
+				if(dir[e] != 0)
+				{
+					readSector(buffer, dir[e]);
+					buffer += 512;
+					++*sectorsRead;
+					//*sectorsRead = *sectorsRead+1;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		entryCount++;
+	}
+}
+
+void executeProgram(char* name)
+{
+	int sectorsRead=0;
+	char buffer[13312];
+	int address;
+	readFile(name, buffer, &sectorsRead);
+
+	//this prevents trying to execute a file that doesn't exist
+	if(sectorsRead !=  0)
+	{
+		for(address = 0; address < 13312; address++)
+		{
+			putInMemory(0x2000, address, buffer[address]);
+		}
+		launchProgram(0x2000);
+	}
+}
+
+void terminate()
+{
+	char shell[6];
+	shell[0] = 's';
+	shell[1] = 'h';
+	shell[2] = 'e';
+	shell[3] = 'l';
+	shell[4] = 'l';
+	shell[5] = '\0';
+	executeProgram(shell);
+	while(1);
+}
+
+void writeSector(char* buffer, int sector)
+{
+        int track = 0;
+        int head = 0;
+        int relSector = sector+1;
+        interrupt(0x13, 3*256+1, buffer, track*256+relSector,head*256+0x80);
+}
+
+void deleteFile(char* name)
+{
+	int length = 0;
+	int entry;
+	int entryCount = 0;
+	char dir[512];
+	char map[512];
+
+	readSector(dir, 2);
+	readSector(map, 1);
+
+	  while(name[length] != '\0')
+        {
+                length++;
+
+        }
+
+
+	for(entry = 0; entry < 512; entry = entry + 32)
+	{
+		if(compareFileNames(name, dir, length, entryCount) == 1)
+		{
+			int e;
+			dir[entry] = '\0';
+
+                        for(e=entry+6; e < (entry + 32); e++)
+			{
+				map[dir[e]] = 0; //= 0??
+			}
+		}
+		entryCount++; 
+	}
+	writeSector(dir, 2);
+	writeSector(map, 1);
+
+}
+
+void writeFile(char* buffer, char* name, int numberOfSectors)
+{
+	char map[512];
+	char directory[512];
+	int i;
+	int k;
+	int x;
+	int e;
+
+//load the map and directory sectors
+	readSector(map,1);
+	readSector(directory,2);
+//find a free directory entry and copy the name to that entry 
+	for(i = 0; i < 512; i = i+32 )
+	{
+		if(directory[i] == '\0')
+		{
+			for(e=0; e<32; e++)
+			{
+				//fills all 32 bytes with \0 in case file name
+				//is less than 6
+				directory[i+e] = '\0';
+			}
+			for(e=0; e<6; e++)
+			{
+				directory[i+e] = name[e];
+			}
+			break;
+		}
+	}
+
+//find a free sector by searching through the map for a 0 starting at 3
+//sets that sector to 0xFF in the Map
+//Adds that sector number to the file's directory entry
+//Writes 512 bytes from the buffer holding the file to that sector
+		for(k=3; k<512; k++)
+		{
+			if(map[k] == 0 && numberOfSectors != 0)
+			{
+				map[k] = 0xFF;
+				directory[i+6] = k;
+				writeSector(buffer, k);
+				buffer += 512;
+
+				writeSector(map, 1);
+				writeSector(directory, 2);
+
+				numberOfSectors--;
+				i++; //goes to next byte in directory entry
+			}
+		}
+	return;
+}
+
+int compareFileNames(char* file1, char* file2, int length, int entryCount)
+{
+	int i;
+	entryCount = entryCount*32;
+
+        for(i=0; i<length; i++)
+        {
+                if( file1[i] != file2[entryCount+i])
+                {
+                        return 0;
+                }
+        }
+        return 1;
+}
+
+void handleTimerInterrupt(int segment, int sp)
+{
+	//printChar('T');
+	//printChar('i');
+	//printChar('c');
+
+	returnFromTimer(segment, sp);
+}
---
+++ shell.c
@@ -0,0 +1,213 @@
+//James Kerester and Molly Croke
+main()
+{
+	while(1)
+	{
+
+		char line[80];
+	//	syscall(0, line); //test to see whats in line
+		syscall(0, "SHELL>");
+		syscall(1, line);
+	//	syscall(0, line); //test to see whats in line
+
+		// "type" command
+		if(line[0]=='t' && line[1]=='y' && line[2]=='p' && line[3]=='e')
+		{
+			int sectorsRead=0;
+			int i=0;
+			int m;
+			char buffer[13312];
+			char fileName[7];
+
+			for(m=0; m<7; m++)
+			{
+				fileName[m] = '\0';
+			}
+			//puts anything typed after "type" into an array
+
+			while(line[i+5] != 0xa)
+			{
+				fileName[i] = line[i+5]; 
+				i++;
+			}
+
+
+
+			//calls readFile to load the file into buffer
+			syscall(3, fileName, buffer, &sectorsRead);
+			//prints the file if found
+			if(sectorsRead !=  0)
+			{
+				syscall(0, buffer);
+				syscall(0, "\r");
+			}
+			else
+			{
+				syscall(0, "File not found.\r\n");
+			}
+		}
+		//"exec" command
+		else if(line[0]=='e' && line[1]=='x' && line[2]=='e' && line[3]=='c')
+		{
+			int i=0;
+			int m;
+			char fileName[7];
+
+			 for(m=0; m<7; m++)
+                        {
+                                fileName[m] = '\0';
+                        }
+
+			  while(line[i+5] != 0xa)
+                        {
+                                fileName[i] = line[i+5]; 
+                                i++;
+                        }
+			//calls executeProgram to execute the file typed
+			syscall(4, fileName); 
+			syscall(0, "File not found.\r\n");
+
+		}
+		else if(line[0]=='d' && line[1]=='i' && line[2]=='r')
+		{
+			int entry;
+			char dir[512];
+			syscall(2, dir, 2);
+
+			for(entry = 0; entry < 512; entry = entry + 32)
+			{
+				if(dir[entry] != '\0')
+				{
+					char fileName2[7];
+					//need to terminate file name
+					//make sure filename is 7 long
+					//put the terminate filename after
+					//the char fileName2[7] = 0?? or \0
+					int i;
+					for(i=0; i<6; i++)
+					{
+						fileName2[i] = dir[entry+i];
+					}
+					fileName2[6] = '\0';
+					syscall(0, fileName2);
+					syscall(0, "\r\n");
+				}
+			}
+
+
+		}
+		else if(line[0]=='d' && line[1]=='e' && line[2]=='l')
+		{
+			int i=0;
+			int m;
+			char fileName[7];
+
+			 for(m=0; m<7; m++)
+                        {
+                                fileName[m] = '\0';
+                        }
+
+		          while(line[i+4] != 0xa)
+                        {
+                                fileName[i] = line[i+4]; 
+                                i++;
+                        }
+			syscall(7, fileName);
+		}
+		else if(line[0]=='c' && line[1]=='o' && line[2]=='p' && line[3]=='y')
+		{
+			int sectorsRead=0;
+			int i=0;
+			int e=0;
+			int x;
+			int z;
+			char buffer[13312];
+			char fileName[7];
+			char fileName2[7];
+
+			for(x=0; x<7; x++)
+                        {
+                                fileName[x] = '\0';
+                        }
+
+			for(z=0; z<7; z++)
+                        {
+                                fileName2[z] = '\0';
+                        }
+
+			while(line[i+5] != ' ')
+			{
+				fileName[i] = line[i+5];
+				i++;
+			}
+			//syscall(0, fileName); //test
+
+			syscall(3, fileName, buffer, &sectorsRead);
+
+
+			while(line[i+6] != 0xa)
+			{
+				fileName2[e] = line[i+6];
+				i++;
+				e++;
+			}
+		//	syscall(0,fileName2);//test
+		//	syscall(0, buffer);//test
+
+			syscall(8, buffer, fileName2, sectorsRead); 
+		}
+		else if(line[0]=='c' && line[1]=='r' && line[2]=='e' && line[3]=='a' && line[4]=='t' && line[5]=='e')
+		{
+			int buffTotal=0;
+			int x;
+			int i=0;
+			char textLine[80];
+			char buffer[13312];
+			char fileName[7];
+
+			for(x=0; x<7; x++)
+			{
+				fileName[x] = '\0';
+			}
+			while(line[i+7] != 0xa)
+			{
+				fileName[i] = line[i+7];
+				i++;
+			}
+
+
+			syscall(1, textLine);
+			i=0;
+			while(textLine[i] != 0xa)
+			{
+				buffer[i] = textLine[i];
+				i++;
+			}
+		//	buffer[buffTotal+i+1]=0x0D; //"\r"
+                  //    buffer[buffTotal+i+2]=0x0A; //"\n"
+                        buffTotal+=i;
+
+			while(textLine[0] != 0x0A)
+			{
+				i=0;
+				syscall(1, textLine);
+				while(textLine[i] != 0xa)
+				{
+					buffer[buffTotal+i] = textLine[i];
+					i++;
+				}
+			//	buffer[buffTotal+i+1]=0x0D; //"\r"
+			//	buffer[buffTotal+i+2]=0x0A; //"\n"
+				buffTotal+=i;
+			}
+			//syscall(0, buffer);//test
+			syscall(8, buffer, fileName, 1);
+		}
+		else
+		{
+			syscall(0, "Command not recognized.\r\n");
+		}
+		//this terminates and calls for shell prompt again
+		syscall(5);
+	}
+}
